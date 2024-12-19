e7eed6b14b02c61a9ab0002b3db3d5b3cd882643
nrybowski <nicolas.rybowski@uclouvain.be>
2024-12-19 16:34:38 +0100

    Use version to tag images

--- generate.py
+++ generate.py
@@ -6,12 +6,13 @@ from yaml import load, BaseLoader
 from jinja2 import FileSystemLoader, Environment, select_autoescape
 
 def usage() -> str:
-    return """Usage: <context> <compose>
+    return """Usage: <context> <compose> <version>
 context: path to the context
 compose: output path for the compose file
+version: the version used to tag the images
     """
 
-if len(sys.argv) != 3:
+if len(sys.argv) != 4:
     print(usage())
     exit(1)
 
@@ -20,12 +21,15 @@ if not os.path.exists(context) or not os.path.isfile(context):
     print("Context file not found.")
     exit(1)
 compose = sys.argv[2]
+version = sys.argv[3]
+version = "latest" if version == "master" else version
 
 env = Environment(loader=FileSystemLoader("."), autoescape=select_autoescape(), trim_blocks=True, lstrip_blocks=True)
 template = env.get_template("compose.tmpl")
 
 with open(context, "r") as fd:
     context = load(fd, BaseLoader)
+context['version'] = version
 
 with open(compose, "w") as fd:
     fd.write(template.render(context))
